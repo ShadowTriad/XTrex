5679be6b8a0999ae3162d8e01ec126f79698e086
tzporthouse <32957735+tzporthouse@users.noreply.github.com>
2018-02-14 13:34:39 +0000

    Add files via upload

---
+++ XTrex.java
@@ -0,0 +1,42 @@
+/**
+ * Write a description of class XTrex here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.awt.Color;
+import java.awt.TextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+
+public class XTrex extends JFrame
+{
+    int northPos;
+    int westPos;
+    mode currentMode;
+    int lengthJourney;
+    
+    /**
+     * Constructor for objects of class XTrex
+     */
+    public XTrex()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        return y;
+    }
+}
---
+++ mode.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class mode - write a description of the enum class here
+ * 
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum mode
+{
+    OnOff, Menu, WhereTo, TripComputer, Map, Speech, Satallite, About
+}
