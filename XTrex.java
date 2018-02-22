06dd4148ff5b9920dbc090ffb78678b99c3a5d9d
Shadow Triad <32957571+ShadowTriad@users.noreply.github.com>
2018-02-22 19:27:41 +0000

    Now speechs after adding speech screen:

--- XTrex.java
+++ XTrex.java
@@ -1,46 +1,66 @@
+import javax.swing.*;
+
 /**
  * Main class that displays the XTrex, along with it's screens.
  *
  * @version 22/02/18
  * @author Chris Tyson
+ * @author Faith Yemofio
+ * @author Jasmine Cooke
+ * @author Oonagh Madden-Wells
+ * @author Tilly Porthouse
  */
 
 import javax.swing.*;
+
 import java.awt.*;
 
-public class XTrex extends JFrame {
-
-    public XTrex() {
-
-        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        setSize(727, 727);
-        setResizable(false);
-        setTitle("XTrex");
-
-        setContentPane( new JLabel( new ImageIcon("img/xtrex.jpg") ) );
-        getContentPane().setLayout(null);
-
+public class XTrex extends JFrame
+{
+    public XTrex ()
+    {
+        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+        
+        setSize (727, 727);
+        
+        setResizable (false);
+        
+        setTitle ("XTrex");
+		
+        setContentPane (new JLabel (new ImageIcon ("img/xtrex.jpg")));
+        
+        getContentPane ().setLayout (null);
     }
-
-    private void showScreen( JPanel scr ) {
-        //getContentPane().setLayout(null);
-        getContentPane().removeAll();
-        this.add(scr);
-
-        scr.setSize(new Dimension(240, 353));
-        scr.setBounds(240, 210, 240, 353);
-
-        this.setVisible(true);
+	
+    private void showScreen (JPanel panel)
+    {
+        getContentPane ().removeAll ();
+        
+        this.add (panel);
+		
+        panel.setSize (new Dimension (240, 353));
+        
+        panel.setBounds (240, 260, 240, 353);
+		
+        this.setVisible (true);
     }
-
-    public static void main( String[] argv ) {
-        XTrex xt = new XTrex();
-
-        Satellite sat = new Satellite();
-        About ab = new About();
-        Speech spe = new Speech();
-
-        xt.showScreen( spe );
-        spe.selectButton();
+		
+    public static void main (String [] array)
+    {
+        XTrex XTrex = new XTrex ();
+        
+        About about = new About ();
+        
+        Satellite satellite = new Satellite ();
+		
+        Speech speech = new Speech ();
+        
+        WhereTo whereTo = new WhereTo ();
+		
+		TripComputer tripComputer = new tripComputer ();
+		
+        XTrex.showScreen (speech);
+        
+        speech.selectButton ();
     }
 }
