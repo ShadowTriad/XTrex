bb374ad133e07201a78c17257ea952f43102da2e
Shadow Triad <32957571+ShadowTriad@users.noreply.github.com>
2018-03-08 15:26:22 +0000

    Add files via upload

--- TripComputer.java
+++ TripComputer.java
@@ -7,6 +7,8 @@ import javax.swing.*;
 
 import java.awt.*;
 
+import java.lang.Math.*;
+
 /**
  * In Trip Computer mode the screen displays the users trip odometer, speed and moving time.
  * At the moment the screen displays a random trip odometer, speed and moving time using default values.
@@ -17,31 +19,100 @@ import java.awt.*;
 
 class TripComputer extends JPanel
 {
-	//stores a random value for the trip odometer
+	private static GPS gps = new GPS ();
 	
-	private static String tripOdometer = "0.86";
+	private static int startingLatitude = 0;
 	
-	//stores a random value for the speed
+	private static int startingLongitude = 0;
+		
+	private static int tripOdometer = 0;
+		
+	private static int speed = 0;
+		
+	private static int startingTimeMinutes = 0;
 	
-	private static String speed = "7.00";
+	private static int startingTimeSeconds = 0;
 	
-	//stores a random value for the moving time
+	private static int movingTimeMinutes = 0;
 	
-	private static String movingTimeMinutes = "027";
-	
-	private static String movingTimeSeconds = "08";
+	private static int movingTimeSeconds = 0;
 	
 	//sets up the class so the mode can be displayed on the screen of the XTrex
 	
 	public TripComputer ()
 	{
+		string latitude = gps.getLatitude ();
 		
+		string longitude = gps.getLongitude ();
+		
+		startingLatitude = Integer.parseInt (latitude);
+		
+		startingLongitude = Integer.parseInt (longitude);
+		
+		String time = gps.getTime ();
+		
+		startingTimeMinutes = getMinutes (time)
+		
+		startingTimeSeconds = getSeconds (time)
+	}
+	
+	public int getOdometer (String latitude, String longitude)
+	{
+		String differenceLatitude = (Integer.parseInt (latitude) - Integer.parseInt (startingLatitude)).toRadians ();
+		
+		String differenceLongitude = (Integer.parseInt (longitude) - Integer.parseInt (startingLongitude)).toRadians ();
+		
+		odometer = Math.sin (differenceLatitude / 2) * Math.sin (differenceLatitude / 2) + Math.cos (startingLatitude) * Math.cos (latitude) * Math.sin (differenceLongitude / 2) * Math.sin (differenceLongitude / 2)
+	}
+	
+	public int getMinutes (String time)
+	{
+		String hours = time.substring (0, 2);
+		
+		String minutes = time.substring (2, 4);
+		
+		return Integer.parseInt (hours) * 60 + Integer.parseInt (minutes);
+	}
+	
+	public String getSeconds (String time)
+	{
+		String seconds = time.substring (4);
+		
+		return Integer.parseInt (seconds);
 	}
 	
 	//paint component method is used to continuously display the screen on the XTrex
 	
 	public void paintComponent (Graphics graphics)
 	{
+		string latitude = gps.getLatitude ();
+		
+		string longitude = gps.getLongitude ();
+		
+		tripOdometer = tripOdometer (latitude, longitude)
+		
+		String time = gps.getTime ();
+				
+		int minutes = getMinutes (String time);
+		
+		int seconds = getSeconds (String time);
+		
+		if (minutes > startingTimeMinutes)
+		{
+			movingTimeMinutes = minutes - startingTimeMinutes;
+			
+			movingTimeSeconds = seconds - startingTimeSeconds;
+		}
+		
+		else
+		{
+			movingTimeMinutes = minutes + 24 * 60 - startingTimeMinutes;
+			
+			movingTimeSeconds = seconds + 24 * 3600 - startingTimeSeconds;
+		}
+		
+		speed = tripOdometer / (movingTimeMinutes / 60 + movingTimeSeconds / 3600)
+		
 		graphics.setColor(Color.black);
 		
 		graphics.fillRect(0, 0, 240, 353);
--- XTrex.java
+++ XTrex.java
@@ -1,206 +1,3 @@
-<<<<<<< HEAD
-/**
- * @version 22/02/18
- * @author Chris Tyson
- * @author Faith Yemofio
- * @author Jasmine Cooke
- * @author Oonagh Madden-Wells
- * @author Tilly Porthouse
- */
-
-import javax.swing.*;
-
-import java.awt.*;
-
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import javax.swing.border.LineBorder;
-
-public class XTrex extends JFrame
-{
-
-    final OnOffButton onoff = new OnOffButton();
-    final PlusMinusButton plus = new PlusMinusButton("+");
-    final PlusMinusButton minus = new PlusMinusButton("-");
-    
-    final SelectButton select = new SelectButton("SELECT");
-    
-    final MenuButton menu = new MenuButton("MENU");
-    
-    static mode currentMode; //purely symbolic for now, does not change what appears on screen - @Oonagh
-
-    public XTrex ()
-    {
-        setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-
-        setSize (727, 727);
-
-        setResizable (false);
-
-        setTitle ("XTrex");
-
-        setContentPane (new JLabel (new ImageIcon ("img/xtrex.jpg")));
-
-        getContentPane ().setLayout (null);
-
-        onoff.setBounds( 417, 114, 62, 62);
-        add(onoff);
-
-        plus.setBounds(155, 70, 40, 50);
-        add(plus);
-
-        minus.setBounds(155, 135, 40, 50);
-        add(minus);
-        
-        select.setBounds(145, 225, 60, 70);
-        add(select);
-        
-        menu.setBounds(515, 75, 50, 70);
-        add(menu);
-    }
-
-    /*
-     * Not sure if this is the way in which we'll represent the mode being changed but this is just a suggestion for now.
-     * 
-     * @author Oonagh
-     * 
-     */
-    private class OnOffButton extends JButton {
-        OnOffButton() {
-            setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setContentAreaFilled(false);
-            setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        if (currentMode == mode.OnOff) {
-                            currentMode = mode.Menu;
-                        } else {
-                            currentMode = mode.OnOff;
-                        }
-                    }
-                });
-        }
-    }
-
-     /*
-     * This might be a really weird way of doing this. I could just as easily make 'plusbutton' and 'minusbutton' classes seperately but idk if it makes a difference. 
-     * 
-     * @author Oonagh
-     * 
-     */
-    private class PlusMinusButton extends JButton {
-        PlusMinusButton(String text) {
-            super(text);
-            setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setFont(new Font("Arial", Font.BOLD, 16));
-            setForeground(Color.RED);
-            setContentAreaFilled(false);
-            setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        switch (text) {
-                            case "+": 
-                                System.out.println("plus"); 
-                                //behave as appropriate to currentmode
-                                break;
-                            case "-": 
-                                System.out.println("minus");
-                                break;
-                        }
-                    } 
-                });
-        }
-    }
-
-     /*
-     * 
-     * @author Oonagh
-     * 
-     */
-    private class SelectButton extends JButton {
-        SelectButton(String text) {
-            super(text);
-            setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setFont(new Font("Arial", Font.BOLD, 12));
-            setForeground(Color.RED);
-            setContentAreaFilled(false);
-            setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        // do whatever select in this mode does
-                    } 
-                });
-        }
-    }
-    
-     /*
-     * 
-     * @author Oonagh
-     * 
-     */
-    private class MenuButton extends JButton {
-        MenuButton(String text) {
-            super(text);
-            setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setFont(new Font("Arial", Font.BOLD, 12));
-            setForeground(Color.RED);
-            setContentAreaFilled(false);
-            setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        currentMode = mode.Menu;
-                    } 
-                });
-        }
-    }
-    
-    /*
-     * Takes different mode's screen as argument and display on screen.
-     */ 
-    private void showScreen (JPanel panel)
-    {
-        //getContentPane ().removeAll (); //need a solution to this
-
-        this.add (panel);
-
-        panel.setSize (new Dimension (240, 353));
-
-        panel.setBounds (240, 260, 240, 353);
-
-        this.setVisible (true);
-    }
-
-    /*
-     * Currently can check modes manually by changing XTrex.showScreen(arg) argument.
-     * Speech mode used here as example; to check other modes, change arg and
-     * comment out speech.selectButton();.
-     */
-    public static void main (String [] array)
-    {
-        currentMode = mode.OnOff;
-        XTrex XTrex = new XTrex ();
-
-        About about = new About ();
-
-        Map map = new Map ();
-
-        Satellite satellite = new Satellite ();
-
-        Speech speech = new Speech ();
-
-        TripComputer tripComputer = new TripComputer ();
-
-        WhereTo whereTo = new WhereTo ();
-
-        XTrex.showScreen (about);
-
-        //speech.selectButton ();
-    }
-=======
 /**
  * @version 22/02/18
  * @author Chris Tyson
@@ -219,15 +16,14 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.border.LineBorder;
-
 
 public class XTrex extends JFrame
 {
     final OnOffButton onOffButton = new OnOffButton();
     
-    final PlusMinusButton plusButton = new PlusMinusButton ("+");
+    final PlusButton plusButton = new PlusButton ("+");
     
-    final PlusMinusButton minusButton = new PlusMinusButton ("-");
+    final MinusButton minusButton = new MinusButton ("-");
     
     final SelectButton selectButton = new SelectButton ("SELECT");
     
@@ -245,7 +41,7 @@ public class XTrex extends JFrame
 	
     static WhereTo whereTo = new WhereTo ();
     
-    static mode currentMode; //purely symbolic for now, does not change what appears on screen - @Oonagh
+    static mode currentMode; //purely symbolic for now, does not change what appears on screen
 	
     public XTrex ()
     {
@@ -281,118 +77,150 @@ public class XTrex extends JFrame
         
         add (menuButton);
     }
-
+	
     /*
-     * Not sure if this is the way in which we'll represent the mode being changed but this is just a suggestion for now.
-     * 
      * @author Oonagh
-     * 
      */
-    private class OnOffButton extends JButton {
-        OnOffButton() {
-            setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setContentAreaFilled(false);
+    
+    private class OnOffButton extends JButton
+    {
+    	OnOffButton ()
+    	{
+    		setBorder(new LineBorder(Color.RED));
+    		
+    		setContentAreaFilled(false);
+    		
             setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        if (currentMode == mode.OnOff) {
-                            currentMode = mode.Menu;
-                        } else {
-                            currentMode = mode.OnOff;
-                        }
+            
+            addMouseListener ( new MouseAdapter ()
+            {
+            	public void mouseClicked (MouseEvent evenet)
+            	{
+                    if (currentMode == mode.OnOff)
+                    {
+                    	currentMode = mode.Menu;
                     }
-                });
+                    
+                    else
+                    {
+                        currentMode = mode.OnOff;
+                    }
+                }
+            });
         }
     }
-
-     /*
-     * This might be a really weird way of doing this. I could just as easily make 'plusbutton' and 'minusbutton' classes seperately but idk if it makes a difference. 
-     * 
+	
+    /*
      * @author Oonagh
-     * 
+     * @author Faith
      */
-    private class PlusMinusButton extends JButton {
-        PlusMinusButton(String text) {
-            super(text);
+    
+    private class PlusButton extends JButton
+    {
+        PlusButton (String plus)
+        {
             setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setFont(new Font("Arial", Font.BOLD, 16));
-            setForeground(Color.RED);
-            setContentAreaFilled(false);
+    		
+    		setContentAreaFilled(false);
+    		
             setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        switch (text) {
-                            case "+": 
-                                if (currentMode == mode.WhereTo)
-                                {
-                                	whereTo.plus ();
-                                }
-                                
-                            case "-": 
-                                if (currentMode == mode.WhereTo)
-                                {
-                                	whereTo.plus ();
-                                }
-                        }
-                    } 
-                });
-        }
-    }
-
-     /*
-     * 
-     * @author Oonagh
-     * 
-     */
-    private class SelectButton extends JButton {
-        SelectButton(String text) {
-            super(text);
-            setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setFont(new Font("Arial", Font.BOLD, 12));
-            setForeground(Color.RED);
-            setContentAreaFilled(false);
-            setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        // do whatever select in this mode does
-                    } 
-                });
+            
+            addMouseListener ( new MouseAdapter ()
+            {
+            	public void mouseClicked (MouseEvent evenet)
+            	{
+                    if (currentMode == mode.WhereT0)
+                    {
+                    	whereTo.plus ();
+					}
+                }
+            });
         }
     }
     
-     /*
-     * 
+    /*
      * @author Oonagh
-     * 
+     * @author Faith
      */
-    private class MenuButton extends JButton {
-        MenuButton(String text) {
-            super(text);
+    
+    private class MinusButton extends JButton
+    {
+        MinusButton (String plus)
+        {
             setBorder(new LineBorder(Color.RED));
-            //setBorderPainted(false);
-            setFont(new Font("Arial", Font.BOLD, 12));
-            setForeground(Color.RED);
-            setContentAreaFilled(false);
+    		
+    		setContentAreaFilled(false);
+    		
             setOpaque(false);
-            addMouseListener( new MouseAdapter() {
-                    public void mouseClicked( MouseEvent me) {
-                        currentMode = mode.Menu;
-                    } 
-                });
+            
+            addMouseListener ( new MouseAdapter ()
+            {
+            	public void mouseClicked (MouseEvent evenet)
+            	{
+                    if (currentMode == mode.WhereT0)
+                    {
+                    	whereTo.minus ();
+					}
+                }
+            });
+        }
+    }
+
+    /*
+     * @author Oonagh
+     */
+     
+    private class SelectButton extends JButton
+    {
+        SelectButton (String plus)
+        {
+            setBorder(new LineBorder(Color.RED));
+    		
+    		setContentAreaFilled(false);
+    		
+            setOpaque(false);
+            
+            addMouseListener ( new MouseAdapter ()
+            {
+            	public void mouseClicked (MouseEvent evenet)
+            	{
+					
+                }
+            });
+        }
+    }
+    
+    /*
+     * @author Oonagh
+     */
+    
+    private class MenuButton extends JButton
+    {
+        MenuButton (String plus)
+        {
+            setBorder(new LineBorder(Color.RED));
+    		
+    		setContentAreaFilled(false);
+    		
+            setOpaque(false);
+            
+            addMouseListener ( new MouseAdapter ()
+            {
+            	public void mouseClicked (MouseEvent evenet)
+            	{
+                	currentMode = mode.Menu;
+                }
+            });
         }
     }
     
     /*
      * Takes different mode's screen as argument and display on screen.
-     */ 
+     */
+     
     private void showScreen (JPanel panel)
     {
-        //getContentPane ().removeAll (); //need a solution to this
-
-        this.add (panel);
+		this.add (panel);
 
         panel.setSize (new Dimension (240, 353));
 
@@ -406,6 +234,7 @@ public class XTrex extends JFrame
      * Speech mode used here as example; to check other modes, change arg and
      * comment out speech.selectButton();.
      */
+    
     public static void main (String [] array)
     {
         currentMode = mode.OnOff;
@@ -416,5 +245,4 @@ public class XTrex extends JFrame
 		
         //speech.selectButton ();
     }
->>>>>>> c3c8c3bc65742d77bb625cd63aa3b66d05cede27
 }
