9f2e9b5ca3823966c795a4ecfc06027ce83a4a0a
Shadow Triad <32957571+ShadowTriad@users.noreply.github.com>
2018-03-21 23:28:31 +0000

    Add files via upload

--- TripComputerView.java
+++ TripComputerView.java
@@ -14,8 +14,8 @@ import java.util.Observable;
 import java.util.Observer;
 import javax.swing.*;
 
-//extends ModeView and implements Observer so the class can use model view controller
-public class TripComputer extends ModeView implements Observer
+//extends ModelView and implements Observer so the class can use model view controller
+public class TripComputer extends ModelView implements Observer
 {
 	public XTrexController controller;
 	public XTrexModel model;
@@ -38,20 +38,20 @@ public class TripComputer extends ModeView implements Observer
 	public void paintComponent (Graphics graphics)
 	{
 		graphics.setColor(Color.black);
-		graphics.fillRect(0, 0, mode.getConstant.getXTrexWidth (), mode.getConstant.getXTrexHeight ());
-		(new ImageIcon (mode.getConstant.getMeterBackground ())).paintIcon(this, graphics, 0, mode.getConstant.getTripComputerBackgroundRow ());
+		graphics.fillRect(0, 0, model.getConstant.getXTrexWidth (), model.getConstant.getXTrexHeight ());
+		(new ImageIcon (model.getConstant.getMeterBackground ())).paintIcon(this, graphics, 0, model.getConstant.getTripComputerBackgroundRow ());
 		graphics.setColor (Color.black);	
-		graphics.setFont (new Font (mode.getConstant.getTextFont (), Font.PLAIN, mode.getConstant.getTextSize ()));
-		mode.updateCoordinates ();
-		mode.updateTripOdometer ();
-		mode.updateMovingTime ();
-		mode.updateSpeed ();
-		graphics.drawString (mode.getConstant.getTripOdometer (), mode.getConstant.getTripComputerTextColumn1 (), mode.getConstant.getTripComputerTextRow1 ());
-		graphics.drawString (mode.getTripOdometer + mode.getConstant.getDistanceUnits (), mode.getConstant.getTripComputerTextColumn2 (), mode.getConstant.getTripComputerTextRow2 ());
-		graphics.drawString (mode.getConstant.getSpeed (), mode.getConstant.getTripComputerTextColumn3 (), mode.getConstant.getTripComputerTextRow3 ());
-		graphics.drawString (mode.getSpeed + mode.getConstant.getSpeedUnits (), mode.getConstant.getTripComputerTextColumn4 (), mode.getConstant.getTripComputerTextRow4 ());
-		graphics.drawString (mode.getConstant.getMovingTime (), mode.getConstant.getTripComputerTextColumn5 (), mode.getConstant.getTripComputerTextRow5 ());
-		graphics.drawString (mode.getMovingTimeMinutes + mode.getConstant.getTimeUnits1 () + mode.getMovingTimeSeconds + mode.getConstant.getTimeUnits2 (), mode.getConstant.getTripComputerTextColumn6 (), mode.getConstant.getTripComputerTextRow6 ());
+		graphics.setFont (new Font (model.getConstant.getTextFont (), Font.PLAIN, model.getConstant.getTextSize ()));
+		model.updateCoordinates ();
+		model.updateTripOdometer ();
+		model.updateMovingTime ();
+		model.updateSpeed ();
+		graphics.drawString (model.getConstant.getTripOdometer (), model.getConstant.getTripComputerTextColumn1 (), model.getConstant.getTripComputerTextRow1 ());
+		graphics.drawString (model.getTripOdometer + model.getConstant.getDistanceUnits (), model.getConstant.getTripComputerTextColumn2 (), model.getConstant.getTripComputerTextRow2 ());
+		graphics.drawString (model.getConstant.getSpeed (), model.getConstant.getTripComputerTextColumn3 (), model.getConstant.getTripComputerTextRow3 ());
+		graphics.drawString (model.getSpeed + model.getConstant.getSpeedUnits (), model.getConstant.getTripComputerTextColumn4 (), model.getConstant.getTripComputerTextRow4 ());
+		graphics.drawString (model.getConstant.getMovingTime (), model.getConstant.getTripComputerTextColumn5 (), model.getConstant.getTripComputerTextRow5 ());
+		graphics.drawString (model.getMovingTimeMinutes + model.getConstant.getTimeUnits1 () + model.getMovingTimeSeconds + model.getConstant.getTimeUnits2 (), model.getConstant.getTripComputerTextColumn6 (), model.getConstant.getTripComputerTextRow6 ());
 		repaint ();
 	}
 }
