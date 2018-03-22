8fa7edb4be3ddd921b4e786544b4f0b256b4e253
jgcooke <36491015+jgcooke@users.noreply.github.com>
2018-03-22 12:37:11 +0000

    Update MenuView.java

--- MenuView.java
+++ MenuView.java
@@ -10,31 +10,31 @@ import java.awt.event.*;
 import java.util.Observable;
 import java.util.Observer;
 
-public class MenuView extends JPanel implements Observer {
+public class MenuView extends JPanel implements Observer{
 	private XTrexController controller;
 	private XTrexModel model;
 	
 	
-	public MenuView( XTrexController controller, XTrexModel model) {
+	public MenuView(XTrexController controller, XTrexModel model){
 		this.controller = controller;
 		this.model = model;
 		
-		model.addObserver( this );
+		model.addObserver(this);
 		setLayout(null);
 		setBackground(new Color(0,0,0));
 		setVisible(true);
 
 	}
 
-	public void paintComponent( Graphics g ) {
+	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.black);
 		g.fillRect(0,0, 330, 415);
-		(new ImageIcon ("img/tilesWhite.jpeg")).paintIcon(this, g, 0, 20); //move to constants
+		(new ImageIcon("img/tilesWhite.jpeg")).paintIcon(this, g, 0, 20); //move to constants
 		int x = 0;
 		int y = 0;
 
-		if(model.getIndex() % 2 == 0) {
+		if(model.getIndex() % 2 == 0){
 			x = 0;
 			y = (model.getIndex()/2 * 97) + 20;
 		}
@@ -47,7 +47,7 @@ public class MenuView extends JPanel implements Observer {
 		
 	}
 	
-	public void update( Observable obs, Object obj ) {
+	public void update(Observable obs, Object obj){
 		repaint();
 	}
 }
