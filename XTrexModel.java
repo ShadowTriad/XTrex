7013df79e48b43fa2e9a08d4913e1e7b8c11bdad
Shadow Triad <32957571+ShadowTriad@users.noreply.github.com>
2018-03-23 04:13:17 +0000

    Update XTrexModel.java

--- XTrexModel.java
+++ XTrexModel.java
@@ -35,10 +35,51 @@ public class XTrexModel extends Observable
 	private boolean goodRead;
 
 	//WhereTo
-	
-	private String destination = "THE FORUM EXETER";
+	private String address = "";
 	private int highlightedButton = 0;
-	private Keyboard keyboardMode = Keyboard.ALPHABETIC;
+	private Keyboard keyboard = Keyboard.ALPHABETIC;
+	private ArrayList <Keycap> alphabeticButtons = new ArrayList <Keycap> (constant.getAlphabeticButtons ());
+	private ArrayList <Keycap> numericButtons = new ArrayList <Keycap> (constant.getNumericButtons ());
+	private Keycap buttonA = new Keycap ("A", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow1 ());
+	private Keycap buttonB = new Keycap ("B", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow1 ());
+	private Keycap buttonC = new Keycap ("C", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow1 ());
+	private Keycap buttonD = new Keycap ("D", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow1 ());
+	private Keycap buttonE = new Keycap ("E", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow2 ());
+	private Keycap buttonF = new Keycap ("F", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow2 ());
+	private Keycap buttonG = new Keycap ("G", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow2 ());
+	private Keycap buttonH = new Keycap ("H", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow2 ());
+	private Keycap buttonI = new Keycap ("I", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow3 ());
+	private Keycap buttonJ = new Keycap ("J", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow3 ());
+	private Keycap buttonK = new Keycap ("K", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow3 ());
+	private Keycap buttonL = new Keycap ("L", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow3 ());
+	private Keycap buttonM = new Keycap ("M", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow4 ());
+	private Keycap buttonN = new Keycap ("N", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow4 ());
+	private Keycap buttonO = new Keycap ("O", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow4 ());
+	private Keycap buttonP = new Keycap ("P", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow4 ());
+	private Keycap buttonQ = new Keycap ("Q", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow5 ());
+	private Keycap buttonR = new Keycap ("R", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow5 ());
+	private Keycap buttonS = new Keycap ("S", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow5 ());
+	private Keycap buttonT = new Keycap ("T", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow5 ());
+	private Keycap buttonU = new Keycap ("U", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow6 ());
+	private Keycap buttonV = new Keycap ("V", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow6 ());
+	private Keycap buttonW = new Keycap ("W", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow6 ());
+	private Keycap buttonX = new Keycap ("X", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow6 ());
+	private Keycap buttonY = new Keycap ("Y", constant.getAlphabeticColumn1 (), constant.getAlphabeticRow7 ());
+	private Keycap buttonZ = new Keycap ("Z", constant.getAlphabeticColumn2 (), constant.getAlphabeticRow7 ());
+	private Keycap button_ = new Keycap ("_", constant.getAlphabeticColumn3 (), constant.getAlphabeticRow7 ());
+	private Keycap buttonRIG = new Keycap ("RIG", constant.getAlphabeticColumn4 (), constant.getAlphabeticRow7 ());
+	private Keycap button1 = new Keycap ("1", constant.getNumericColumn1 (), constant.getNumericRow1 ());
+	private Keycap button2 = new Keycap ("2", constant.getNumericColumn2 (), constant.getNumericRow1 ());
+	private Keycap button3 = new Keycap ("3", constant.getNumericColumn3 (), constant.getNumericRow1 ());
+	private Keycap button4 = new Keycap ("4", constant.getNumericColumn1 (), constant.getNumericRow2 ());
+	private Keycap button5 = new Keycap ("5", constant.getNumericColumn2 (), constant.getNumericRow2 ());
+	private Keycap button6 = new Keycap ("6", constant.getNumericColumn3 (), constant.getNumericRow2 ());
+	private Keycap button7 = new Keycap ("7", constant.getNumericColumn1 (), constant.getNumericRow3 ());
+	private Keycap button8 = new Keycap ("8", constant.getNumericColumn2 (), constant.getNumericRow3 ());
+	private Keycap button9 = new Keycap ("9", constant.getNumericColumn3 (), constant.getNumericRow3 ());
+	private Keycap button0 = new Keycap ("0", constant.getNumericColumn1 (), constant.getNumericRow4 ());
+	private Keycap buttonLEF = new Keycap ("LEF", constant.getNumericColumn1 (), constant.getNumericRow5 ());
+	private Keycap buttonDEL = new Keycap ("DEL", constant.getNumericColumn2 (), constant.getNumericRow4 ());
 
 	//Trip Compute attributes
 	
@@ -69,6 +110,46 @@ public class XTrexModel extends Observable
 	public XTrexModel (Mode mode)
 	{
 		updateCoordinates();
+		alphabeticButtons.add (buttonA);
+		alphabeticButtons.add (buttonB);
+		alphabeticButtons.add (buttonC);
+		alphabeticButtons.add (buttonD);
+		alphabeticButtons.add (buttonE);
+		alphabeticButtons.add (buttonF);
+		alphabeticButtons.add (buttonG);
+		alphabeticButtons.add (buttonH);
+		alphabeticButtons.add (buttonI);
+		alphabeticButtons.add (buttonJ);
+		alphabeticButtons.add (buttonK);
+		alphabeticButtons.add (buttonL);
+		alphabeticButtons.add (buttonM);
+		alphabeticButtons.add (buttonN);
+		alphabeticButtons.add (buttonO);
+		alphabeticButtons.add (buttonP);
+		alphabeticButtons.add (buttonQ);
+		alphabeticButtons.add (buttonR);
+		alphabeticButtons.add (buttonS);
+		alphabeticButtons.add (buttonT);
+		alphabeticButtons.add (buttonU);
+		alphabeticButtons.add (buttonV);
+		alphabeticButtons.add (buttonW);
+		alphabeticButtons.add (buttonX);
+		alphabeticButtons.add (buttonY);
+		alphabeticButtons.add (buttonZ);
+		alphabeticButtons.add (button_);
+		alphabeticButtons.add (buttonRIG);
+		numericButtons.add (button1);
+		numericButtons.add (button2);
+		numericButtons.add (button3);
+		numericButtons.add (button4);
+		numericButtons.add (button5);
+		numericButtons.add (button6);
+		numericButtons.add (button7);
+		numericButtons.add (button8);
+		numericButtons.add (button9);
+		numericButtons.add (button0);
+		numericButtons.add (buttonLEF);
+		numericButtons.add (buttonDEL);
 		setStartingTime (getTime ());
 		setMode (mode);
 	}
@@ -150,6 +231,133 @@ public class XTrexModel extends Observable
 		setChanged();
 		notifyObservers();
 
+	}
+	
+	//Where To
+	
+	public String getAddress ()
+	{
+		return address;
+	}
+	
+	public int getHighlightedButton ()
+	{
+		return highlightedButton;
+	}
+	
+	public Keyboard getKeyboard ()
+	{
+		return keyboard;
+	}
+	
+	public ArrayList <Keycap> getAlphabeticButtons ()
+	{
+		return alphabeticButtons;
+	}
+	
+	public ArrayList <Keycap> getNumericButtons ()
+	{
+		return numericButtons;
+	}
+	
+	public void whereToPlusButton ()
+	{
+		if (keyboard == Keyboard.ALPHABETIC)
+		{
+			if (highlightedButton == constant.getAlphabeticButtons () - 1)
+			{
+				highlightedButton = 0;
+			}
+			else
+			{
+				highlightedButton = highlightedButton + 1;
+			}
+		}
+		else
+		{
+			if (highlightedButton == constant.getNumericButtons () - 1)
+			{
+				highlightedButton = 0;
+			}
+			else
+			{
+				highlightedButton = highlightedButton + 1;
+			}
+			setChanged ();
+			notifyObservers ();
+		}
+	}
+	
+	public void whereToMinusButton ()
+	{
+		if (keyboard == Keyboard.ALPHABETIC)
+		{
+			if (highlightedButton == 0)
+			{
+				highlightedButton = constant.getAlphabeticButtons () - 1;
+			}
+			else
+			{
+				highlightedButton = highlightedButton - 1;
+			}
+		}
+		else
+		{
+			if (highlightedButton == 0)
+			{
+				highlightedButton = constant.getNumericButtons () - 1;
+			}
+			else
+			{
+				highlightedButton = highlightedButton - 1;
+			}
+		}
+		setChanged ();
+		notifyObservers ();
+	}
+	
+	public void whereToSelectButton ()
+	{
+		if (keyboard == Keyboard.ALPHABETIC)
+		{		
+			if ((alphabeticButtons.get (highlightedButton)).getName () == "RIG")
+			{
+				keyboard = Keyboard.NUMERIC;
+				highlightedButton = 0;
+			}
+			else
+			{
+				address = address + (alphabeticButtons.get (highlightedButton)).getName ();
+			}
+		}
+		else
+		{
+			if ((numericButtons.get (highlightedButton)).getName () == "DEL")
+			{			
+				if (address.length () == 0)
+				{
+				}
+				else if (address.length () == 1)
+				{
+					address = "";
+				}
+				else
+				{
+					address = address.substring (0, address.length () - 1);
+				}
+			}
+			else if ((numericButtons.get (highlightedButton)).getName () == "LEF")
+			{
+				keyboard = Keyboard.ALPHABETIC;
+				highlightedButton = 0;
+			}
+			else
+			{
+				address = address + (numericButtons.get (highlightedButton)).getName ();
+			}
+		}
+		setChanged ();
+		notifyObservers ();
 	}
 	
        /*
@@ -239,104 +447,6 @@ public class XTrexModel extends Observable
 		movingTimeSeconds = seconds % constant.getMinuteSeconds ();
 		setChanged ();
 		notifyObservers ();
-	}
-
-	//Trip Computer methods
-	
-	public Keyboard getKeyboardMode ()
-	{
-		return this.keyboardMode;
-	}
-
-	public String getDestination ()
-	{
-		return this.destination;
-	}
-
-	public int getHighlightedButton ()
-	{
-		return this.highlightedButton;
-	}
-
-	public void incrementHighlighted ()
-	{
-		this.highlightedButton += 1;
-
-		int limit;
-		if (keyboardMode == Keyboard.ALPHABETIC)
-		{
-			limit = constant.getAlphabeticButtons ();
-		}
-		else
-		{
-			limit = constant.getNumericButtons ();
-		}
-
-		if (highlightedButton == limit)
-		{
-			this.highlightedButton = 0;
-		}
-		setChanged ();
-		notifyObservers ();
-	}
-
-	public void decrementHightlighted ()
-	{
-		this.highlightedButton -= 1;
-		int limit;
-		if (keyboardMode == Keyboard.ALPHABETIC)
-		{
-			limit = constant.getAlphabeticButtons ();
-		}
-		else
-		{
-			limit = constant.getNumericButtons ();
-		}
-
-		if (highlightedButton == -1)
-		{
-			this.highlightedButton = limit - 1;
-		}
-
-		setChanged ();
-		notifyObservers ();
-	}
-
-	public void changeKeyboard() {
-		if (keyboardMode == Keyboard.NUMERIC) {
-			this.keyboardMode = Keyboard.ALPHABETIC;
-		} else {
-			this.keyboardMode = Keyboard.NUMERIC;
-		}
-		this.highlightedButton = 0;
-
-		setChanged();
-		notifyObservers();
-	}
-
-	public void addLetter() {
-		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
-		this.destination += alphabet.charAt( highlightedButton );
-
-		setChanged();
-		notifyObservers();
-	}
-
-	public void addNumber() {
-		String nums = "1234567890";
-		this.destination += nums.charAt( highlightedButton );
-
-		setChanged();
-		notifyObservers();
-	}
-
-	public void delFromAddress() {
-		if ( this.destination.length() > 0 ) {
-			this.destination = destination.substring(0, destination.length()-1);
-		}
-
-		setChanged();
-		notifyObservers();
 	}
 
 	/*
@@ -483,7 +593,7 @@ public class XTrexModel extends Observable
           String lat = latitude;
           String lon = longitude;
 
-          String encDestination = URLEncoder.encode( destination, "UTF-8" );
+          String encDestination = URLEncoder.encode( address, "UTF-8" );
           String language = languages.get(currentLanguageCount);
 
           //fortunately the directions API just needs the first two characters of these language inputs used in the speech api.
