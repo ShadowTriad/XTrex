a8a368ffc588724b6c3e8b8cd12ad62b13a2b53c
oonaghmw <32957755+oonaghmw@users.noreply.github.com>
2018-03-23 02:43:22 +0000

    improved map (can rotate n zoom n stuff)

--- MapView.java
+++ MapView.java
@@ -1,61 +1,62 @@
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.LineBorder;
-
 import java.util.Observable;
 import java.util.Observer;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class MapView extends ModeView implements Observer {
 	public XTrexController controller;
 	public XTrexModel model;
+	private String latitude;
+	private String longitude;
+	
+	private int zoom; 
+	private ImageIcon map;
+	private Direction last;
+	private double rad;
 	
 	public MapView(XTrexController controller, XTrexModel model) {
 		this.controller = controller;
 		this.model = model;
+		this.latitude = model.getLatitude();
+		this.longitude = model.getLongitude();
+		
+		this.zoom = model.getZoom();
+		this.map = model.getMap();
+		this.rad = 0;
 		
 		model.addObserver( this );
 		repaint();
 	}
 	
-	/*
-     * Use when + button is pressed. Functionality to update image must be added.
-     *
-	public void zoomIn() {
-        if (zoom > 0){
-            zoom -= 1; 
-            strZoom = Integer.toString(zoom);
-        }
-    }
-	
-	 /*
-      * Use when - button is pressed. Functionality to update image must be added.
-      *
-    public void zoomOut() {
-        if (zoom < 0) {
-            zoom += 1; 
-            strZoom = Integer.toString(zoom);
-        }
-    }
-	*/
-	
-	public void update( Observable obs, Object obj ) {
-		System.out.println(obs);
-	}
-	
 	private void redDot(Graphics g) {
         g.setColor(Color.red);
-        int dotSize = Integer.parseInt(model.getZoom())/2;
-        g.fillOval(Constants.SCREENWIDTH/2-dotSize/2, Constants.SCREENHEIGHT/2-dotSize/2, dotSize, dotSize); 
+        int dotSize = zoom/2;
+        g.fillOval(Constants.SCREENWIDTH/2 - dotSize/2, Constants.SCREENHEIGHT/2 - dotSize/2, dotSize, dotSize); 
     }
 	
 	/*
      * Method to display map on screen with red marker in centre.
      */
-    protected void paintComponent( Graphics g ) {
+    public void paintComponent( Graphics g ) {
 		super.paintComponent(g);
-	
-		System.out.println(model.getMap());
-        model.getMap().paintIcon(this, g, 0, 0);
-        this.redDot(g);
+        Graphics2D g2 = (Graphics2D) g;
+        g2.rotate(rad, Constants.SCREENWIDTH/2, Constants.SCREENHEIGHT/2);
+		
+        map.paintIcon(this, g2, Constants.SCREENWIDTH/2 - 250, Constants.SCREENHEIGHT/2 - 250); //add to constants		
+        this.redDot(g2);
     }
+	
+	public void update( Observable obs, Object obj ) {
+		rad = model.getRadians();
+		System.out.println(rad);
+		
+		zoom = model.getZoom();
+		this.map.getImage().flush();
+       
+		repaint();
+	}
 }
